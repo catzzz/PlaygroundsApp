4a97b08eb96dd5e8bbc9086bd9f5ee2a5e1d49ce
Catzzz <chuneleu@gmail.com>
2021-07-23 10:55:30 -0700

    config passport

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const methodOverride = require('method-override');
 const session = require('express-session');
 const ExpressError = require('./utils/ExpressError');
 const ejsMate = require('ejs-mate');
-
-
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user')
 const flash = require('connect-flash');
+
+
+
 
 // routes
 const playgrounds = require('./routes/playgrounds');
@@ -37,6 +41,20 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 // end session config
 
+// config passport start
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+// save user in session
+passport.serializeUser(User.serializeUser());
+// get user outof session
+passport.deserializeUser(User.deserializeUser());
+
+
+//config passport end
+
+
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -63,6 +81,12 @@ app.use('/playgrounds', playgrounds);
 app.use('/playgrounds/:id/reviews', reviews);
 
 
+// text user 
+app.get('/fakeUser', async(req, res)=>{
+    const user = new User({email:'chuneleu@gmail.com',username:'jimmy'});
+    const newUser = await User.register(user,'chicken');
+    res.send(newUser);
+})
 
 
 // Home
