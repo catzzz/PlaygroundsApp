e6f1406b6963e14a54f975b9aaa4803324b2fd51
Catzzz <chuneleu@gmail.com>
2021-07-23 16:27:08 -0700

    show and hide edit / delete in show playground page

--- models/playground.js
+++ models/playground.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const Review = require('./review')
+const Review = require('./review');
+const User = require('./user')
 const Schema = mongoose.Schema;
 
 const PlaygroundSchema = new Schema({
@@ -8,6 +9,10 @@ const PlaygroundSchema = new Schema({
     image: String,
     description: String,
     location: String,
+    author:{
+        type:Schema.Types.ObjectId,
+        ref:'User'
+    },
     reviews: [
         {
             type: Schema.Types.ObjectId,
--- routes/playgrounds.js
+++ routes/playgrounds.js
@@ -37,6 +37,7 @@ router.post('/',isLoggedIn, validatePlayground, catchAsync(async (req, res, next
  
     if(!req.body.playground) throw new ExpressError('Invalid Playground Data',400);
     const playground = new Playground(req.body.playground);
+    playground.author = req.user._id;
     await playground.save();
     req.flash('success', 'Successfully made a new playground!');
     res.redirect(`/playgrounds/${playground._id}`);
@@ -46,7 +47,8 @@ router.post('/',isLoggedIn, validatePlayground, catchAsync(async (req, res, next
 // Show single Playground
 
 router.get('/:id', catchAsync(async (req, res, next) => {
-    const playground = await Playground.findById(req.params.id).populate('reviews');
+    const playground = await Playground.findById(req.params.id).populate('reviews').populate('author');
+
     if (!playground) {
         req.flash('error', 'Cannot find that playground!');
         return res.redirect('/playgrounds');
