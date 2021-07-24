0a0c82a0f84d701976e77da14dfe453f9a0cfc63
Catzzz <chuneleu@gmail.com>
2021-07-24 13:55:26 -0700

    restruct routes with router.route

--- routes/playgrounds.js
+++ routes/playgrounds.js
@@ -1,34 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const playgrounds = require('../controllers/playgrounds');
-const catchAsync = require('../utils/catchAsync');
-const { playgroundSchema } = require('../schemas.js');
-const ExpressError = require('../utils/ExpressError');
-const Playground = require('../models/playground');
-const { isLoggedIn,validatePlayground, isAuthor } = require('../middleware');
+const playgrounds = require("../controllers/playgrounds");
+const catchAsync = require("../utils/catchAsync");
+const { playgroundSchema } = require("../schemas.js");
+const ExpressError = require("../utils/ExpressError");
+const Playground = require("../models/playground");
+const { isLoggedIn, validatePlayground, isAuthor } = require("../middleware");
+
+router.route("/")
+  // List all playgrounds
+  .get(catchAsync(playgrounds.index))
+  // Add new Playground api
+  .post(
+    isLoggedIn,
+    validatePlayground,
+    catchAsync(playgrounds.createPlayground)
+  );
 
 
+// New
+router.get("/new", isLoggedIn, playgrounds.renderNewPlaygroundPage);
 
+router.route("/:id")
+   // Show single Playground
+  .get(catchAsync(playgrounds.showPlayground))
+  // Edit single Playground
+  .put(
+    isLoggedIn,
+    isAuthor,
+    validatePlayground,
+    catchAsync(playgrounds.updatePlayground)
+  )
+  // Delete single playground
+  .delete(
+    isLoggedIn,
+    isAuthor,
+    catchAsync(playgrounds.deletePlayground)
+  );
 
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isAuthor,
+  catchAsync(playgrounds.renderEditPlaygroundPage)
+);
 
-// List all playgrounds
-router.get('/', catchAsync(playgrounds.index));
-
-// New 
-
-router.get('/new',isLoggedIn, playgrounds.renderNewPlaygroundPage );
-
-// Add new Playground api
-router.post('/',isLoggedIn, validatePlayground, catchAsync(playgrounds.createPlayground))
-
-// Show single Playground
-
-router.get('/:id', catchAsync(playgrounds.showPlayground));
-
-// Show edit single Playground
-router.get('/:id/edit',isLoggedIn, isAuthor, catchAsync(playgrounds.renderEditPlaygroundPage))
-// Edit single Playground
-router.put('/:id',isLoggedIn, isAuthor, validatePlayground, catchAsync(playgrounds.updatePlayground))
-
-router.delete('/:id', isLoggedIn, isAuthor,catchAsync( playgrounds.deletePlayground))
 module.exports = router;
