0acbe021d0dc245d95268a90bad5c3ef1401383f
Catzzz <chuneleu@gmail.com>
2021-07-28 15:39:27 -0700

    add sanitize-html in joi

--- schemas.js
+++ schemas.js
@@ -1,11 +1,34 @@
-const Joi = require("joi");
+const BaseJoi = require("joi");
+const sanitizeHtml = require('sanitize-html');
+// Joi extension to protect cross site attack
+const extension = (joi) => ({
+  type: 'string',
+  base: joi.string(),
+  messages: {
+      'string.escapeHTML': '{{#label}} must not include HTML!'
+  },
+  rules: {
+      escapeHTML: {
+          validate(value, helpers) {
+              const clean = sanitizeHtml(value, {
+                  allowedTags: [],
+                  allowedAttributes: {},
+              });
+              if (clean !== value) return helpers.error('string.escapeHTML', { value })
+              return clean;
+          }
+      }
+  }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 //Validation form through Joi
 module.exports.playgroundSchema = Joi.object({
   playground: Joi.object({
-    title: Joi.string().required(),
-    location: Joi.string().required(),
-    description: Joi.string().required(),
+    title: Joi.string().required().escapeHTML(),
+    location: Joi.string().required().escapeHTML(),
+    description: Joi.string().required().escapeHTML(),
   }).required(),
   deleteImages: Joi.array()
 });
@@ -14,6 +37,6 @@ module.exports.playgroundSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating:Joi.number().required(),
-        body:Joi.string().required()
+        body:Joi.string().required().escapeHTML()
     }).required()
 })
