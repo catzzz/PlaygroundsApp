4f4bed0b57bf58561554f7d7bff1b8c34b392b6a
Catzzz <chuneleu@gmail.com>
2021-07-28 15:39:27 -0700

    add sanitize-html in joi

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -5,16 +5,13 @@ const mobileMenuBtnClose = document.getElementById('mobile-menu-btn-close');
 const navbar = document.getElementById('navbar');
 if(navbar){
     const route = window.location.pathname;
-    console.log(route);
+  
     if(route === '/'){
       
         navbar.style.backgroundColor ="transparent";
         navbar.style.borderBottom ="1px solid white";
         
-    }else{
-
     }
-
 }
 
 
